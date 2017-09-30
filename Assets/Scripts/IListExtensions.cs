eecb1c292b2b0f181762920abfad7d83124c7b03
Alistair Broomhead <alistair.broomhead@gmail.com>
2017-09-30 15:19:02 +0100

    Added handy list extensions

---
+++ Assets/Scripts/IListExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solitaire.Game.IListExtensions
+{
+    public static class IListExtensions
+    {
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int count = list.Count;
+            int last = count - 1;
+
+            for (int i = 0; i < last; i++)
+            {
+                int r = UnityEngine.Random.Range(i, count);
+                T tmp = list[i];
+                list[i] = list[r];
+                list[r] = tmp;
+            }
+        }
+        public static T PopAt<T>(this List<T> list, int index)
+        {
+            if (index >= list.Count)
+                return default(T);
+
+            T value = list[index];
+            list.RemoveAt(index);
+
+            return value;
+        }
+        public static T Pop<T>(this List<T> list)
+        {
+            return list.PopAt(list.Count - 1);
+        }
+    }
+}
